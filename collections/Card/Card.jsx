6ca2f2a910e1f56ada93b020c2b70ae5b181c199
Nikita <7masteryakali@gmail.com>
2023-03-24 10:31:57 +0200

    add Card layout

--- collections/Card/Card.jsx
+++ collections/Card/Card.jsx
@@ -1,1 +1,22 @@
 // The Card to be exported goes here
+import Image from "next/image";
+import { StyledDescription } from "../../sections/Hero/elements";
+import {} from "./elements"
+export const Card=({icon,title,description,...props})=>{
+    return(
+        <StyledCardContainer>
+            <StyledIconContainer>
+                <Image>
+                    src={icon.src}
+                    alt={icon.alt}
+                    width={icon.width}
+                    height={icon.height}
+                </Image>
+            </StyledIconContainer>
+            <StyledTextContainer>
+                <StyledTitle>{title}</StyledTitle>
+                <StyledDescription>{description}</StyledDescription>
+            </StyledTextContainer>
+        </StyledCardContainer>
+    );
+}
